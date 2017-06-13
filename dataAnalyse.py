07412f9151c821e609b479370480a10f1c23246a
pankun <pankun1992@gmail.com>
2017-06-13 21:04:11 +0800

    speed split analyse

--- dataAnalyse.py
+++ dataAnalyse.py
@@ -50,7 +50,10 @@ def speedAnalyse_split(listData_point,listData_time):
             cha = numpy.subtract(listData_point[i][j],pre_data)
             pingfang = numpy.square(cha)
             lenth = math.sqrt(numpy.sum(pingfang))
-            speed = lenth/((listData_time[i][j]-pre_time)+0.0001)
+            time_minus = (listData_time[i][j]-pre_time)+0.0001
+            speed = 0.6
+            if time_minus!=0:
+                speed = lenth/time_minus
             pre_data = listData_point[i][j]
             pre_time = listData_time[i][j]
             speed_split.append(speed)
@@ -70,10 +73,50 @@ def speedAnalyse_split(listData_point,listData_time):
 # print ("机器速度中位数：",numpy.median(resultSpeed_robot))
 # print ("机器速度众数：",mode(resultSpeed_robot))
 
-resultSpeed_humain_split = speedAnalyse_split(listData_humain,listData_humain_time)
-list_Speed_split_max = []
-for i in range(len(resultSpeed_humain_split)):
-    if(len(resultSpeed_humain_split[i])>0):
-        list_Speed_split_max.append(numpy.max(resultSpeed_humain_split[i]))
-print(list_Speed_split_max)
+def analyse_split_humain_3():
+    resultSpeed_humain_split = speedAnalyse_split(listData_humain,listData_humain_time)
+    list_median = []
+    for one_speed_humain_split in resultSpeed_humain_split:
+        if(len(one_speed_humain_split)>3):
+            list_medianTmp = []
+            one_speed_humain_split_1 = one_speed_humain_split[0:int(len(one_speed_humain_split)/3)]
+            one_speed_humain_split_2 = one_speed_humain_split[int(len(one_speed_humain_split)/3):int(2*len(one_speed_humain_split)/3)]
+            one_speed_humain_split_3 = one_speed_humain_split[int(2*len(one_speed_humain_split)/3):]
+            list_medianTmp.append(numpy.median(one_speed_humain_split_1))
+            list_medianTmp.append(numpy.median(one_speed_humain_split_2))
+            list_medianTmp.append(numpy.median(one_speed_humain_split_3))
+            list_median.append(list_medianTmp)
+    print (list_median)
+    return list_median
+    
+
+def analyse_split_robot_3():
+    resultSpeed_robot_split = speedAnalyse_split(listData_robot,listData_robot_time)
+    list_median = []
+    for one_speed_robot_split in resultSpeed_robot_split:
+        if(len(one_speed_robot_split)>3):
+            list_medianTmp = []
+            one_speed_robot_split_1 = one_speed_robot_split[0:int(len(one_speed_robot_split)/3)]
+            one_speed_robot_split_2 = one_speed_robot_split[int(len(one_speed_robot_split)/3):int(2*len(one_speed_robot_split)/3)]
+            one_speed_robot_split_3 = one_speed_robot_split[int(2*len(one_speed_robot_split)/3):]
+            list_medianTmp.append(numpy.median(one_speed_robot_split_1))
+            list_medianTmp.append(numpy.median(one_speed_robot_split_2))
+            list_medianTmp.append(numpy.median(one_speed_robot_split_3))
+            list_median.append(list_medianTmp)
+    print (list_median)
+    return list_median
+
+# list_Speed_split_median = []
+# for i in range(len(resultSpeed_humain_split)):
+#     if(len(resultSpeed_humain_split[i])>0):
+#         list_Speed_split_median.append(numpy.median(resultSpeed_humain_split[i]))
+# print ()
+
+# print (resultSpeed_humain_split[2])
+# resultSpeed_robot_split = speedAnalyse_split(listData_robot,listData_robot_time)
+# list_Speed_split_median_robot = []
+# for i in range(len(resultSpeed_robot_split)):
+#     if(len(resultSpeed_robot_split[i])>0):
+#         list_Speed_split_median_robot.append(numpy.median(resultSpeed_robot_split[i]))
+# print(list_Speed_split_median_robot)
      
--- lstmTrain.py
+++ lstmTrain.py
@@ -1,14 +1,3 @@
-# View more python learning tutorial on my Youtube and Youku channel!!!
-
-# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
-# Youku video tutorial: http://i.youku.com/pythontutorial
-
-"""
-This code is a modified version of the code from this link:
-https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/recurrent_network.py
-
-His code is a very good one for RNN beginners. Feel free to check it out.
-"""
 import tensorflow as tf
 from dataTraitor import DataTrainSet
 import numpy
@@ -16,21 +5,19 @@ import numpy
 pathName = "D:\\bigData\\dsjtzs_txfz_training.txt"
 pathNameT = "D:\\bigData\\dsjtzs_txfz_test1.txt"
 
-# set random seed for comparing the two result calculations
 tf.set_random_seed(1)
 
-# this is data
 mnist = DataTrainSet(pathName,pathNameT)
 
-# hyperparameters
+
 lr = 0.001
 training_iters = 100000
 batch_size = 100
 
-n_inputs = 6   # MNIST data input (img shape: 28*28)
-n_steps = 300    # time steps
-n_hidden_units = 100   # neurons in hidden layer
-n_classes = 2      # MNIST classes (0-2 digits)
+n_inputs = 6  
+n_steps = 300    
+n_hidden_units = 100   
+n_classes = 2      
 
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
@@ -38,65 +25,29 @@ y = tf.placeholder(tf.float32, [None, n_classes])
 
 # Define weights
 weights = {
-    # (28, 128)
     'in': tf.Variable(tf.random_normal([n_inputs, n_hidden_units])),
-    # (128, 10)
     'out': tf.Variable(tf.random_normal([n_hidden_units, n_classes]))
 }
 biases = {
-    # (128, )
     'in': tf.Variable(tf.constant(0.1, shape=[n_hidden_units, ])),
-    # (10, )
     'out': tf.Variable(tf.constant(0.1, shape=[n_classes, ]))
 }
 
 
 def RNN(X, weights, biases):
-    # hidden layer for input to cell
-    ########################################
-
-    # transpose the inputs shape from
-    # X ==> (128 batch * 28 steps, 28 inputs)
     X = tf.reshape(X, [-1, n_inputs])
-
-    # into hidden
-    # X_in = (128 batch * 28 steps, 128 hidden)
     X_in = tf.matmul(X, weights['in']) + biases['in']
-    # X_in ==> (128 batch, 28 steps, 128 hidden)
     X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])
-
-    # cell
-    ##########################################
-
-    # basic LSTM Cell.
     if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
         cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
     else:
         cell = tf.contrib.rnn.BasicLSTMCell(n_hidden_units)
-    # lstm cell is divided into two parts (c_state, h_state)
     init_state = cell.zero_state(batch_size, dtype=tf.float32)
-
-    # You have 2 options for following step.
-    # 1: tf.nn.rnn(cell, inputs);
-    # 2: tf.nn.dynamic_rnn(cell, inputs).
-    # If use option 1, you have to modified the shape of X_in, go and check out this:
-    # https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/recurrent_network.py
-    # In here, we go for option 2.
-    # dynamic_rnn receive Tensor (batch, steps, inputs) or (steps, batch, inputs) as X_in.
-    # Make sure the time_major is changed accordingly.
-    outputs, final_state = tf.nn.dynamic_rnn(cell, X_in, initial_state=init_state, time_major=False)
-
-    # hidden layer for output as the final results
-    #############################################
-    # results = tf.matmul(final_state[1], weights['out']) + biases['out']
-
-    # # or
-    # unpack to list [(batch, outputs)..] * steps
     if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
-        outputs = tf.unpack(tf.transpose(outputs, [1, 0, 2]))    # states is the last outputs
+        outputs = tf.unpack(tf.transpose(outputs, [1, 0, 2]))    
     else:
         outputs = tf.unstack(tf.transpose(outputs, [1,0,2]))
-    results = tf.matmul(outputs[-1], weights['out']) + biases['out']    # shape = (128, 10)
+    results = tf.matmul(outputs[-1], weights['out']) + biases['out']  
 
     return results
 
@@ -114,8 +65,6 @@ predict = tf.nn.softmax(pred)
 
 
 with tf.Session() as sess:
-    # tf.initialize_all_variables() no long valid from
-    # 2017-03-02 if using tensorflow >= 0.12
     if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
         init = tf.initialize_all_variables()
     else:
